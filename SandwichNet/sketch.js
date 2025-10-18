87cc006d32d0a71c3f2fca090f2dfe10b749918a
Felix Sewing <felix.sewing@hfg-gmuend.de>
2025-10-18 15:14:02 +0200

    add function for touc screens

--- SandwichNet/sketch.js
+++ SandwichNet/sketch.js
@@ -535,7 +535,13 @@ function mouseClicked()
   {
     generateNewSandwich();
   }
-  
+}
+
+function touchStarted() {
+  if(touch.x < w/2 + 75 && touch.x > w/2 - 75 && touch.y < h-100 + 35 && touch.y > h-100 - 35 )
+  {
+    generateNewSandwich();
+  }
 }
 
 function startTraining()
